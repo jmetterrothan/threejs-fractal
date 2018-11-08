7dc71c258b81b31c545d9708ea55b58a741c13b5
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-11-08 15:22:14 +0100

    Added UI state selector

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ const scene = new Scene();
 
 scene.init();
 scene.start();
+
+//  Change scene w/ the ui select element
+const $uiSceneSelector = document.getElementById('uiScene');
+
+$uiSceneSelector.addEventListener('change', () => {
+    const index = parseInt($uiSceneSelector.value, 10);
+    scene.switchState(index);
+});
